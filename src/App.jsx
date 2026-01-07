e0034da2ffaed1e9e87c7ed11c54c14bc92ca69d
saikirankotichintala <saikirankotichintala@gmail.com>
2026-01-07 20:31:31 +0530

    Fix imports to match component filenames

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Skills from "./components/Skills";
-import About from "./components/About";
-import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact"; // 
+
 
 function App() {
   return (
