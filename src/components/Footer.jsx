f89388ddb7422c167c15e0339c9aa354161d8773
saikirankotichintala <saikirankotichintala@gmail.com>
2026-01-08 22:49:52 +0530

    changed linkedln

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <a href="https://saikirankotichintala.vercel.app" target="_blank" rel="noreferrer">
             Portfolio
           </a>
-          <a href="https://www.linkedin.com/in/saikiran-kotichintala/" target="_blank" rel="noreferrer">
+          <a href="https://www.linkedin.com/in/saikirankotichintalala/" target="_blank" rel="noreferrer">
             LinkedIn
           </a>
           <a href="https://github.com/saikirankotichintala" target="_blank" rel="noreferrer">
