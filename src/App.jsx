12a8a157ccf0d8ba640196ac31c789269deeb9c8
saikirankotichintala <saikirankotichintala@gmail.com>
2026-01-08 22:38:08 +0530

    changed footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact"; 
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 
@@ -12,12 +13,11 @@ function App() {
     <>
       <Navbar />
       <Hero />
-<About />
-<Skills />
-<Projects />
+      <About />
+      <Skills />
+      <Projects />
       <Contact />
       <Footer />
-
     </>
   );
 }
